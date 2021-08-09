d33e7bc6f0649fdbd92fe482dbab994be8fd3962
rohilpinto <rohilcrispinto@gmail.com>
2021-08-09 19:56:35 +0530

    fixed btn

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -11,7 +11,7 @@ const Input = ({ number, setNumber, filterData, setFilterData, data, prevData, s
     console.log(toNumber);
     console.log(number);
     if (toNumber <= 0) {
-      toNumber = 1;
+      toNumber = 0;
     }
 
     if (number >= 8) {
@@ -23,15 +23,15 @@ const Input = ({ number, setNumber, filterData, setFilterData, data, prevData, s
   // console.log(newdata);
   return (
     <div className="user-input">
-      <form action="" className="form" onSubmit={handleSubmit}>
+      <form action="" className="form">
         <div className="input-container inputs">
-          <div className="user-input-container">
+          <div className="user-input-container" onChange={(e) => setNumber(e.target.value)}>
             <label htmlFor="number">Number of paragraphs </label>
-            <input type="number" id="number" value={number} onChange={(e) => setNumber(e.target.value)} />
+            <input type="number" id="number" value={number} />
           </div>
         </div>
 
-        <div className="user-submit inputs">
+        <div className="user-submit inputs" onClick={handleSubmit}>
           <label htmlFor="">
             <button type="submit" value="submit">
               <img src={EnterIcon} alt="enter-icon" name="enter-icon" />
